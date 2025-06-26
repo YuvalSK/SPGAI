e1ea95c46eb6474c34884a9169f739667f2d6187
YuvalSK <samoilov.y@gmail.com>
2025-06-26 12:44:39 +0300

    updated analysis

--- playground.py
+++ playground.py
@@ -19,31 +19,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 srcs = [Danenberg2022, Jackson2020, Keren2018] 
-
-src = srcs[2]
-
+src = srcs[0]
 src_name = str(src).split(".")[-1][:-2]
 print(f"Dataset: {src_name}")
-
 base_dir = Path(f"c://Users//User/Projects/Epigenetics/{src_name}")  # can be None, resolves to ~/.cache/ai4bmr_datasets by default
+
 dataset = src(base_dir)
 
 # issues:
-    ## Danenbergh2022 - this runs an R script to convert file types (manual in windows)
-    ### complete! 
-    
+    ## Danenbergh2022 - complete (manually run an R script to convert file types) 
     ## Jackson2020 - this runs an error with pandas formating (series and Int32 indexing)
     # FileNotFoundError: [Errno 2] No such file or directory: 'c:\\Users\\User\\Projects\\Epigenetics\\Jackson2020\\02_processed\\metadata\\published'
-    
     ## Keren2019
     # DeflateError: libdeflate_zlib_decompress returned LIBDEFLATE_INSUFFICIENT_SPACE
 dataset.prepare_data()  # only needs to be run once
 
 dataset.setup(image_version="published", mask_version="published")
-
-print(len(dataset.sample_ids))    # List of sample IDs
-#print(dataset.images.keys()) # Dictionary of images
-#print(dataset.masks.keys())  # Dictionary of masks
 
 dataset.setup(
     image_version="published",
@@ -52,16 +43,18 @@ dataset.setup(
     metadata_version="published", load_metadata=True
 )
 
-print(dataset.intensity.shape)  # Cell x marker matrix
-print(dataset.metadata.shape)   # Cell x annotation matrix
+#### example properties:
+#print(len(dataset.sample_ids))    # List of sample IDs
+#print(dataset.images.keys()) # Dictionary of images
+#print(dataset.masks.keys())  # Dictionary of masks
+#print(dataset.intensity.shape)  # Cell x marker matrix
+#print(dataset.metadata.shape)   # Cell x annotation matrix
+#sample_id = dataset.sample_ids[0]  # get the first sample ID
+#img = dataset.images[sample_id].data
+#print("Image shape:", img.shape)
+#dataset.intensity.to_csv(f"{src_name}.csv")
 
-sample_id = dataset.sample_ids[0]  # get the first sample ID
-img = dataset.images[sample_id].data
-print("Image shape:", img.shape)
-
-##### dimensionality reduction analysis
-#dataset.intensity.to_csv("Danenberg2022.csv")
-# analysis
+#### dimensionality reduction analysis
 # --- Corr ---
 spearman_corr = dataset.intensity.corr(method='spearman')
 plt.figure(figsize=(16, 12))
@@ -95,11 +88,7 @@ plt.savefig(f"Figures/{src_name}-pca-scree.png", dpi=600)
 ## with elbow methods, seems 3 components are not bad => ~30% of variance
 
 
-# 1-PCA
-pca = PCA(n_components=3)
-pca_result = pca.fit_transform(scaled_data)
-
-# reducing
+# reducing dimensions of data
 pca = PCA(n_components=10)
 pca_data = pca.fit_transform(scaled_data)
 
@@ -128,34 +117,54 @@ umap_model = umap.UMAP(n_jobs=4,
 
 umap_result = umap_model.fit_transform(pca_data)
 
-fig, axs = plt.subplots(1, 3, figsize=(18, 5))
+fig, axs = plt.subplots(3, 3, figsize=(18, 5))
 
-# PCA plot - try PC2XPC3?
-axs[0].scatter(pca_result[:, 1], pca_result[:, 2], 
+# PCA plots
+pca = PCA(n_components=3)
+pca_result = pca.fit_transform(scaled_data)
+
+axs[0,0].scatter(pca_result[:, 0], pca_result[:, 1], 
                facecolors='none',
                edgecolors='gray',
                s=10,
                alpha=0.7)
-axs[0].set_xlabel(f'PC2 [A.U.] ({pca.explained_variance_ratio_[1]:.1%} variance)')
-axs[0].set_ylabel(f'PC3 [A.U.] ({pca.explained_variance_ratio_[2]:.1%} variance)')
+axs[0,0].set_xlabel(f'PC1 [A.U.] ({pca.explained_variance_ratio_[0]:.1%} variance)')
+axs[0,0].set_ylabel(f'PC2 [A.U.] ({pca.explained_variance_ratio_[1]:.1%} variance)')
+
+axs[0,1].scatter(pca_result[:, 0], pca_result[:, 2], 
+               facecolors='none',
+               edgecolors='gray',
+               s=10,
+               alpha=0.7)
+axs[0,1].set_xlabel(f'PC1 [A.U.] ({pca.explained_variance_ratio_[0]:.1%} variance)')
+axs[0,1].set_ylabel(f'PC3 [A.U.] ({pca.explained_variance_ratio_[2]:.1%} variance)')
+
+axs[0,2].scatter(pca_result[:, 1], pca_result[:, 2], 
+               facecolors='none',
+               edgecolors='gray',
+               s=10,
+               alpha=0.7)
+axs[0,2].set_xlabel(f'PC2 [A.U.] ({pca.explained_variance_ratio_[1]:.1%} variance)')
+axs[0,2].set_ylabel(f'PC3 [A.U.] ({pca.explained_variance_ratio_[2]:.1%} variance)')
+
 
 # t-SNE plot
-axs[1].scatter(tsne_result[:, 0], tsne_result[:, 1],     
+axs[1,0].scatter(tsne_result[:, 0], tsne_result[:, 1],     
                facecolors='none',
                edgecolors='gray', 
                s=10,
                alpha=0.7)
-axs[1].set_xlabel('t-SNE 1 [A.U.]')
-axs[1].set_ylabel('t-SNE 2 [A.U.]')
+axs[1,0].set_xlabel('t-SNE 1 [A.U.]')
+axs[1,0].set_ylabel('t-SNE 2 [A.U.]')
 
 # UMAP plot
-axs[2].scatter(umap_result[:, 0], umap_result[:, 1], 
+axs[1,1].scatter(umap_result[:, 0], umap_result[:, 1], 
                facecolors='none',
                edgecolors='gray',
                s=10,
                alpha=0.7)
-axs[2].set_xlabel('UMAP 1 [A.U.]')
-axs[2].set_ylabel('UMAP 2 [A.U.]')
+axs[1,1].set_xlabel('UMAP 1 [A.U.]')
+axs[1,1].set_ylabel('UMAP 2 [A.U.]')
 
 plt.tight_layout()
 #plt.show()
