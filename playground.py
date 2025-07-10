467c357c22d5998d8e75cc696e882bc4250a53ba
YuvalSK <samoilov.y@gmail.com>
2025-07-10 18:09:52 +0300

    updated analysis

--- playground.py
+++ playground.py
@@ -4,7 +4,6 @@ Created on Wed Jun 18 18:44:49 2025
 
 @author: YSK
 """
-
 %aimport typing_extensions
 from ai4bmr_datasets import Danenberg2022, Jackson2020, Keren2018
 from pathlib import Path
@@ -13,6 +12,8 @@ import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+
 #from sklearn.manifold import TSNE
 from openTSNE import TSNE #faster
 import umap.umap_ as umap
@@ -48,8 +49,8 @@ dataset.setup(
 dataset.intensity.head()
 dataset.metadata.head()
 
-dataset.intensity.to_parquet(f"{src_name}-intensity.parquet", engine="pyarrow", compression="snappy")
-dataset.metadata.to_parquet(f"{src_name}-metadata.parquet", engine="pyarrow", compression="snappy")
+dataset.intensity.to_parquet(f"{base_dir}/{src_name}-intensity.parquet", engine="pyarrow", compression="snappy")
+dataset.metadata.to_parquet(f"{base_dir}/{src_name}-metadata.parquet", engine="pyarrow", compression="snappy")
 
 #### example properties of the dataset:
 #print(len(dataset.sample_ids))    # List of sample IDs
@@ -62,8 +63,8 @@ dataset.metadata.to_parquet(f"{src_name}-metadata.parquet", engine="pyarrow", co
 #print("Image shape:", img.shape)
 
 #to load the data
-#df = pd.read_parquet(f"{src_name}.parquet", engine="pyarrow")
-#df_meta = pd.read_parquet(f"{src_name}-metadata.parquet", engine="pyarrow")
+df = pd.read_parquet(f"{base_dir}/{src_name}.parquet", engine="pyarrow")
+df_meta = pd.read_parquet(f"{base_dir}/{src_name}-metadata.parquet", engine="pyarrow")
 #print(set(df_meta.label))
 
 # --- Corr ---
@@ -72,13 +73,10 @@ plt.figure(figsize=(16, 12))
 sns.heatmap(spearman_corr, cmap='coolwarm',
             annot_kws={"size": 3},
             vmax = 1, vmin=-1)
-plt.title('Spearman Correlation Heatmap')
+plt.title('Correlation Heatmap (p)')
 plt.savefig(f"Figures/{src_name}-spearman.png", dpi=600)
- 
 
-#### dimensionality reduction analysis
-#normalization (as in Harpaz 2022):
-
+#normalization
 ## 1) Z transform
 scaler = StandardScaler()
 z_data = scaler.fit_transform(df)
@@ -87,11 +85,43 @@ z_data = scaler.fit_transform(df)
 scFac=5
 scaled_data=np.arcsinh(z_data/scFac)
 
+## To do:
+### 3) residual to accont for systematic effects 
+
+# Compute total intensity per cell
+scaled_data = pd.DataFrame(scaled_data)
+scaled_data = scaled_data.sum(axis=1).values.reshape(-1, 1)
+
+# Initialize empty DataFrame for residuals
+df_residuals = pd.DataFrame(index=scaled_data.index, columns=scaled_data.columns)
+
+# For each marker, regress on total intensity and store residuals
+for marker in df.columns:
+    y = df[marker].values.reshape(-1, 1)
+    model = LinearRegression().fit(total_intensity, y)
+    y_pred = model.predict(total_intensity)
+    residuals = (y - y_pred).ravel()
+    df_residuals[marker] = residuals
+    
+spearman_corr_res = df_residuals.corr(method='spearman')
+plt.figure(figsize=(16, 12))
+sns.heatmap(spearman_corr_res, cmap='coolwarm',
+            annot_kws={"size": 3},
+            vmax = 1, vmin=-1)
+plt.title('Residuals Correlation Heatmap (p)')
+plt.savefig(f"Figures/{src_name}-spearman-res.png", dpi=600)
+
+
+#### dimensionality reduction analysis
+#normalization (as in Harpaz 2022):
+
+
+
 pca = PCA()
 pca_result = pca.fit(scaled_data)
 explained_variance = pca.explained_variance_ratio_
 
-def pca_plots(data):  
+def pca_plots(data, file_name):  
     fig, axs = plt.subplots(2, 1, figsize=(10, 6))
     pca = PCA(n_components=2)
     pca_result = pca.fit_transform(scaled_data)
@@ -113,6 +143,9 @@ def pca_plots(data):
     elbow1 = 2
     axs[1].axvline(x=elbow1, c='r', linestyle="dashed", label=f"{explained_variance[:elbow1].sum()*100:.1f}% variation by {elbow1} PCs")
     axs[1].legend(loc='center right')
+    elbow2 = 10
+    axs[1].axvline(x=elbow2, c='k', linestyle="dashed", label=f"{explained_variance[:elbow2].sum()*100:.1f}% variation by {elbow1} PCs")
+    axs[1].legend(loc='center right')
     
     ########## top plot ########## 
     palette = {'epithelial': '#1f77b4', 'non-epithelial': '#ff7f0e'}  # blue & orange
@@ -130,9 +163,9 @@ def pca_plots(data):
     axs[0].set_ylabel(f'PC2 ({pca.explained_variance_ratio_[1]:.1%})')
     
     plt.tight_layout()
-    plt.savefig(f"Figures/{src_name}-pca-norm-main.png", dpi=600)
+    plt.savefig(file_name, dpi=600)
 
-pca_plots(scaled_data)
+pca_plots(scaled_data, f"Figures/{src_name}-pca-norm-res-main.png")
 
 # reducing dimensions of data
 pca = PCA(n_components=10)
@@ -144,14 +177,14 @@ tsne1 = TSNE(n_jobs=2,
             n_components=2,
             perplexity=30, 
             learning_rate=200, 
-            n_iter=300, 
+            n_iter=250, 
             metric="euclidean",
             random_state=1111, 
             verbose=True)
 tsne1_result = tsne1.fit(pca_data)
 
 df = pd.DataFrame(tsne1_result)
-df.to_csv(f'{src_name}-dm-tsne1.csv')
+df.to_csv(f'Results/{src_name}-dm-res-tsne1.csv')
 
 tsne2 = TSNE(n_jobs=2,
             n_components=2,
@@ -164,76 +197,104 @@ tsne2 = TSNE(n_jobs=2,
 tsne2_result = tsne2.fit(pca_data)
 
 df = pd.DataFrame(tsne2_result)
-df.to_csv(f'{src_name}-dm-tsne2.csv')
+df.to_csv(f'Results/{src_name}-dm-res-tsne2.csv')
 
 # 3-UMAP
 umap1_model = umap.UMAP(n_jobs=2,
                        n_components=2, 
                        n_neighbors=15, 
                        min_dist=0.05, 
-                       n_epochs=200,
+                       n_epochs=100,
                        metric='euclidean',
                        verbose=True)
 
 umap1_result = umap1_model.fit_transform(pca_data)
 
 df = pd.DataFrame(umap1_result)
-df.to_csv(f'{src_name}-dm-umap1.csv')
+df.to_csv(f'Results/{src_name}-dm-res-umap1.csv')
 
 umap2_model = umap.UMAP(n_jobs=2,
                        n_components=2, 
                        n_neighbors=45, 
                        min_dist=0.05, 
-                       n_epochs=200,
+                       n_epochs=100,
                        metric='euclidean',
                        verbose=True)
 
 umap2_result = umap2_model.fit_transform(pca_data)
 
 df = pd.DataFrame(umap2_result)
-df.to_csv(f'{src_name}-dm-umap2.csv')
+df.to_csv(f'Results/{src_name}-dm-res-umap2.csv')
 
-#visualization
-fig, axs = plt.subplots(2, 2, figsize=(18, 5))
+def plot_dm(file_name):
+    fig, axs = plt.subplots(2, 2, figsize=(18, 5))
+    
+    # t-SNE plot
+    axs[0,0].scatter(tsne1_result[:, 0], tsne1_result[:, 1],     
+                   facecolors='none',
+                   edgecolors='gray', 
+                   alpha=0.2)
+    axs[0,0].set_xlabel('t-SNE 1 [A.U.]')
+    axs[0,0].set_ylabel('t-SNE 2 [A.U.]')
+    axs[0,0].set_title('t-SNE with perplexity = 30')
+    
+    axs[0,1].scatter(tsne2_result[:, 0], tsne2_result[:, 1],     
+                   facecolors='none',
+                   edgecolors='gray', 
+                   alpha=0.2)
+    axs[0,1].set_xlabel('t-SNE 1 [A.U.]')
+    axs[0,1].set_ylabel('t-SNE 2 [A.U.]')
+    axs[0,1].set_title('t-SNE with perplexity = 50')
+    
+    
+    # UMAP plot
+    axs[1,0].scatter(umap1_result[:, 0], umap1_result[:, 1], 
+                   facecolors='none',
+                   edgecolors='gray',
+                   alpha=0.2)
+    axs[1,0].set_xlabel('UMAP 1 [A.U.]')
+    axs[1,0].set_ylabel('UMAP 2 [A.U.]')
+    axs[1,0].set_title('UMAP with n_neighbors = 15')
+    
+    axs[1,1].scatter(umap2_result[:, 0], umap2_result[:, 1], 
+                   facecolors='none',
+                   edgecolors='gray',
+                   alpha=0.2)
+    axs[1,1].set_xlabel('UMAP 1 [A.U.]')
+    axs[1,1].set_ylabel('UMAP 2 [A.U.]')
+    axs[1,1].set_title('UMAP with n_neighbors = 45')
+    
+    labels = df_meta['label'].unique()
+    palette = {label: color for label, color in zip(labels, sns.color_palette(n_colors=len(labels)))}
+    
+    for label in labels:
+        mask = df_meta['label'].eq(label).to_numpy(dtype=bool)
+        axs[0,0].scatter(tsne1_result[mask, 0], tsne1_result[mask, 1],
+                       alpha=0.2,
+                       label=label,
+                       color=palette[label])  # Fallback color
+        axs[0,1].scatter(tsne2_result[mask, 0], tsne2_result[mask, 1],
+                       alpha=0.2,
+                       label=label,
+                       color=palette[label])
+        axs[1,0].scatter(umap1_result[mask, 0], umap1_result[mask, 1],
+                       alpha=0.2,
+                       label=label,
+                       color=palette[label])
+        axs[1,1].scatter(umap2_result[mask, 0], umap2_result[mask, 1],
+                       alpha=0.2,
+                       label=label,
+                       color=palette[label])
+        
+    handles, labels = axs[0,0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, 1.08),
+               ncol=6, fontsize='x-small', frameon=True)    
+    
+    
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.8)  # Leave space for the legend
 
-# t-SNE plot
-axs[0,0].scatter(tsne1_result[:, 0], tsne1_result[:, 1],     
-               facecolors='none',
-               edgecolors='gray', 
-               alpha=0.2)
-axs[0,0].set_xlabel('t-SNE 1 [A.U.]')
-axs[0,0].set_ylabel('t-SNE 2 [A.U.]')
-axs[0,0].set_title('t-SNE with perplexity = 30')
+    plt.savefig(file_name, dpi=600)
 
-axs[0,1].scatter(tsne2_result[:, 0], tsne2_result[:, 1],     
-               facecolors='none',
-               edgecolors='gray', 
-               alpha=0.2)
-axs[0,1].set_xlabel('t-SNE 1 [A.U.]')
-axs[0,1].set_ylabel('t-SNE 2 [A.U.]')
-axs[0,1].set_title('t-SNE with perplexity = 50')
-
-
-# UMAP plot
-axs[1,0].scatter(umap1_result[:, 0], umap1_result[:, 1], 
-               facecolors='none',
-               edgecolors='gray',
-               alpha=0.2)
-axs[1,0].set_xlabel('UMAP 1 [A.U.]')
-axs[1,0].set_ylabel('UMAP 2 [A.U.]')
-axs[1,0].set_title('UMAP with n_neighbors = 15')
-
-axs[1,1].scatter(umap2_result[:, 0], umap2_result[:, 1], 
-               facecolors='none',
-               edgecolors='gray',
-               alpha=0.2)
-axs[1,1].set_xlabel('UMAP 1 [A.U.]')
-axs[1,1].set_ylabel('UMAP 2 [A.U.]')
-axs[1,1].set_title('UMAP with n_neighbors = 45')
-
-plt.tight_layout()
-plt.show()
-
-plt.savefig(f'Figures/{src_name}-exploration-norm.png', dpi=600)
-
+plot_dm(f'Figures/{src_name}-dr-norm-res-all.png')
 
